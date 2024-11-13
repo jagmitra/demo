5efacba5f0b77269963aeeaa4a4e09425f983273
jagmitra <jagmitra@hotmail.com>
2024-11-13 16:17:59 +0530

    2nd commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("****************************************************************************");
 		System.out.println("welcome this is my demo project");
+		System.out.println("****************************************************************************");
 		SpringApplication.run(DemoApplication.class, args);
+		
 	}
 
 }
